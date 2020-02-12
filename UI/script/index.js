72de39e932394aea639c4dc8f090fc33b6b9810e
Asifiwe <Asifiwe@users.noreply.github.com>
2020-02-12 10:11:24 +0200

    Revert "Revert "feature (hamburger-menu) add""
    This reverts commit 535b22c2d44a820f8b3ce38c54333a8dd5e8c24b.

---
+++ UI/script/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(json());
+app.use(
+    urlencoded({
+        extended: true,
+    }),
+);
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`App running on port ${port}.`);
+});
