139982688f0a1dd3fb3a074e2a09f17fc55a8673
Asifiwe <Asifiwe@users.noreply.github.com>
2020-02-18 07:26:01 +0200

    configure heroku database

--- server/middleware/connect.js
+++ server/middleware/connect.js
@@ -1,8 +1,9 @@
 import { Pool } from 'pg';
 const pool = new Pool({
-    user: 'manzi',
-    database: 'api',
-    password: 'hello',
+    host: '@ec2-54-80-184-43.compute-1.amazonaws.com',
+    user: 'zoauqrnnpxjhwu',
+    database: 'd23f45l9llhci2',
+    password: '1d091e53eab999b06e3ad29709c091ba4b5ddf16deece74f5f2e2ff4e922ec6b',
     port: 5432,
     max: 10,
     idleTimeoutMillis: 30000,
